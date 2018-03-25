eb12de9402d3255dfb09ced345db1cb0ce4434b5
ningmoon <yaozhihao@outlook.com>
2018-03-25 16:43:49 +0800

    1

---
+++ myblog/blog/deploy.sh
@@ -0,0 +1,5 @@
+cp -R public/* .deploy/ningmoon.github.io
+cd .deploy/ningmoon.github.io
+git add .
+git commit -m “update”
+git push 
---
+++ myblog/blog/source/message/index.md
@@ -0,0 +1,7 @@
+---
+title: message
+date: 2018-02-09 22:39:15
+comments: true   打开评论
+---
+
+有事请留言
